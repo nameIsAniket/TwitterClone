92446214777c5ab8b1bd42cbed3524db82a7d0a4
nameIsAniket <aniketlakde7262@gmail.com>
2024-11-10 19:09:58 +0530

    updated frontend of scrolling section

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -5,6 +5,11 @@ import { IoNotificationsOutline } from "react-icons/io5";
 import { LuMail } from "react-icons/lu";
 import { IoBookmarkOutline } from "react-icons/io5";
 import { IoPersonOutline } from "react-icons/io5";
+import { FaRegComment } from "react-icons/fa6";
+import { BiRepost } from "react-icons/bi";
+import { FaRegHeart } from "react-icons/fa";
+import { IoIosStats } from "react-icons/io";
+import { RiShare2Line } from "react-icons/ri";
 
 interface TwitterSideBarItem{
   title : string
@@ -42,7 +47,6 @@ export default function Home() {
   return (
     <div>
       <div className="grid grid-cols-12 h-screen w-screen">
-
         <div className="col-span-3 mt-1 pl-24">
           <div className="text-[26px] hover:bg-gray-900 rounded-full w-fit p-3 cursor-pointer transition-all">
             <BsTwitterX />
@@ -55,10 +59,75 @@ export default function Home() {
               </div>
             ))}
           </div>
-          <button className="font-sans text-xl py-3 mt-2 bg-[#1D9BF0] rounded-full w-52 content-center">Post</button>
+          <button className="font-sans text-xl py-3 mt-2 bg-[#1D9BF0] rounded-full w-52 content-center ">Post</button>
         </div>
 
-        <div className="col-span-5 border-x border-slate-700"></div>
+        <div className="col-span-5 border-x border-slate-700">
+
+          <div className="flex justify-around h-14 items-center border-b border-slate-700 text-slate-400 bg-transparent ">
+            <div>For you</div>
+            <div>Following</div>
+          </div>
+
+          <div className="grid grid-cols-12 px-4 font-sans py-2 border-b border-slate-700">
+            <div className="col-span-1"> </div>
+            <div className="col-span-11 flex flex-col">
+              <div className="flex items-center gap-1">
+                <div>Naval</div>
+                <div className="text-[#71767b]">@naval</div>
+                <div className="bg-[#71767b] rounded-full h-[2px] w-[2px]"><div/></div>
+                <div className="text-[#71767b]">15h</div>
+              </div>
+              
+              <p>
+                Elon Musk wasn’t eligible to be President, so he did the next best thing and got one elected.
+              </p>
+
+              <div className="flex justify-between mt-1">
+                <div className="flex items-center hover:text-[#1D9BF0] ">
+                  <div className=" hover:bg-blue-950 rounded-full p-2">
+                    <FaRegComment />
+                  </div>
+                  <div>330</div>
+                </div>
+
+                <div className="flex items-center hover:text-green-500">
+                  <div className="hover:bg-green-950 rounded-full text-lg p-2">
+                    <BiRepost />
+                  </div>
+                  <div>591</div>
+                </div>
+
+                <div className="flex items-center hover:text-pink-500">
+                  <div className=" hover:bg-pink-950 rounded-full p-2">
+                    <FaRegHeart /> 
+                  </div>
+                  <div>330</div>
+                </div>
+
+                <div className="flex items-center hover:text-[#1D9BF0]">
+                  <div className=" hover:bg-blue-950 rounded-full p-2">
+                    <IoIosStats /> 
+                  </div>
+                  <div>364k</div>
+                </div>
+
+                <div className="flex items-center ">
+                  <div className=" hover:bg-blue-950 rounded-full p-2 hover:text-[#1D9BF0]">
+                    <IoBookmarkOutline />
+                  </div>
+                  <div className=" hover:bg-blue-950 rounded-full p-2 hover:text-[#1D9BF0]">
+                    <RiShare2Line />
+                  </div>
+                </div>
+                
+              </div>
+
+            </div>
+          </div>
+
+        </div>
+
         <div className="col-span-4"></div>
       </div>
     </div>
